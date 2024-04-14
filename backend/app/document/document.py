c7c639f7881dd5c7706ba2478ab41f763a469222
Philipp Hertweck <philipp.hertweck@ocki-software.de>
2024-04-14 23:19:18 +0200

    add semantic scholar similar search

--- backend/app/document/document.py
+++ backend/app/document/document.py
@@ -1,7 +1,7 @@
 import os
 from typing import List
 from app.services.nougat import parsePdfToMardown, ping
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from fastapi import APIRouter, File, HTTPException, UploadFile, status
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
@@ -9,10 +9,23 @@ import requests
 from app.services.text_splitter import Splitter, SplitterOptions, split_text
 from langchain_core.documents import Document
 from app.services.weaviate import WeaviateClient
-from semanticscholar import SemanticScholar
+from semanticscholar import SemanticScholar, PaginatedResults
+from pydantic import BaseModel
+
 
 load_dotenv()
 NOUGAT_URL = os.getenv('NOUGAT_URL')
+
+
+class Document(BaseModel):
+    title: str
+    url: str
+
+
+class SimilarDocumentResponse(BaseModel):
+    message: str = Field(
+        None, description="A message about the result of the operation")
+    similar_docs: List[Document]
 
 
 class DocumentResponse(BaseModel):
@@ -40,7 +53,7 @@ async def getDocument():
              tags=["Document Processing"])
 async def uploadDocument(file: UploadFile = File(...), splitter: Splitter = Splitter.SEMANTIC_TEXT_SPLITTER_MD, chunk_size: int = 2000, chunk_overlap: int = 200, tokenizer_model_name: str = "gpt-4", schema_name: str = "Document"):
     """
-    Uploads a document, converts it to markdown, vectorizes it and stores it in the database.  
+    Uploads a document, converts it to markdown, vectorizes it and stores it in the database.
 
     - **file**: A document file that you want to convert to markdown.
     - **splitter**: The splitter to use for splitting the document into chunks. Default is `Splitter.SEMANTIC_TEXT_SPLITTER_MD`.
@@ -78,7 +91,7 @@ async def uploadDocument(file: UploadFile = File(...), splitter: Splitter = Spli
             tags=["Document Processing"])
 async def ping_nougat():
     """
-    Pings the Nougat service to check its availability.  
+    Pings the Nougat service to check its availability.
 
     This endpoint sends a request to the Nougat service to verify it is up and running. It is useful for automated health checks or for manual verification of the service status.
     """
@@ -90,7 +103,45 @@ async def ping_nougat():
             status_code=503, detail="Nougat service is not available")
 
 
-@router.get("/document/similar")
-async def get_similar_documents(base_document_name: str, num_results: int = 5):
+@router.get("/document/search",
+            summary="Find similar papers using Semantic Scholar API",
+            description="Retrieves a list of similar documents based on the given base document name. This is useful for finding relevant academic papers using Semantic Scholar's data.",
+            response_model=SimilarDocumentResponse,
+            responses={
+                200: {"model": SimilarDocumentResponse, "description": "A list of similar documents is returned."},
+                401: {"description": "API key is missing or invalid"},
+                503: {"description": "Nougat service is not available"},
+            },
+            status_code=status.HTTP_200_OK,
+            tags=["Document Processing"])
+async def get_similar_documents(base_document_name: str, limit: int = 5):
+    """
+     Retrieves a list of similar documents based on the given base document name.
+
+     Args:
+         base_document_name (str): The name of the base document.
+         limit (int, optional): The maximum number of similar documents to retrieve. Defaults to 5.
+
+     Returns:
+         A JSON response with status code 200 and a list of similar documents.
+
+     Raises:
+         HTTPException: If the API key is missing or invalid, returns a 401 Unauthorized status code.
+    """
+    dotEnv = find_dotenv()
+    env = load_dotenv(dotenv_path=dotEnv)
     semantic_scolar_api_key = os.getenv('SEMANTIC_SCOLAR_API_KEY')
-    print(semantic_scolar_api_key)
+    sem_scholar: SemanticScholar
+    if semantic_scolar_api_key is None or semantic_scolar_api_key == "":
+        return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={"message": "API key is missing or invalid"})
+    sem_scholar = SemanticScholar(api_key=semantic_scolar_api_key)
+    similar_documents: PaginatedResults = sem_scholar.search_paper(
+        query=base_document_name, open_access_pdf=True, limit=limit)
+    documents = []
+    for index, item in enumerate(similar_documents.items):
+        document = {
+            "title": item.title,
+            "url": item.openAccessPdf
+        }
+        documents.append(document)
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"documents": documents})
